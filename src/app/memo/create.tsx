134ff25b61c5015dad6c75b430b42570b6eb5d0e
akihiro <a.tokuhara@ntt.com>
2025-05-18 18:00:15 +0900

    96. ユーザーごとにデータを保存する

--- src/app/memo/create.tsx
+++ src/app/memo/create.tsx
@@ -2,16 +2,19 @@ import {
     View, TextInput, StyleSheet, KeyboardAvoidingView
   } from 'react-native'
 import { router } from 'expo-router'
-import { collection, addDoc } from 'firebase/firestore'
+import { collection, addDoc, Timestamp } from 'firebase/firestore'
+import { useState } from 'react'
 
 import CircleButton from '../../components/CircleButton'
 import Icon from '../../components/Icon'
-import { db } from '../../config'
+import { db, auth } from '../../config'
 
-const handlePress = (): void => {
-// const handlePress = async (): void => {
-  addDoc(collection(db, 'memos'), {
-    bodyText: 'test'
+const handlePress = (bodyText: string): void => {
+  if (auth.currentUser === null) {return}
+  const ref = collection(db, `users/${auth.currentUser.uid}/memos`)
+  addDoc(ref, {
+    bodyText,
+    updatedAt: Timestamp.fromDate(new Date())
   })
     .then((docRef) => {
       console.log('success', docRef.id)
@@ -20,21 +23,21 @@ const handlePress = (): void => {
     .catch(() => {
       console.log('error')
     })
-  // await addDoc(collection(db, 'memos'), {
-  //   bodyText: 'test 2'
-  // }).catch((error) => {
-  //     console.log(error)
-  //   })
-  // router.back()
 }
 
 const Create = (): JSX.Element => {
+    const [bodyText, setBodyText] = useState('')
     return (
       <KeyboardAvoidingView behavior='height' style={styles.container}>
         <View style={styles.inputContainer}>
-            <TextInput multiline style={styles.input} value={''} />
+            <TextInput 
+              multiline 
+              style={styles.input} 
+              value={bodyText} 
+              onChangeText={(text) => {setBodyText(text)}}
+            />
         </View>
-        <CircleButton onPress={handlePress}>
+        <CircleButton onPress={() => { handlePress(bodyText) }}>
           <Icon name='check' size={40} color='#ffffff'/>
         </CircleButton>
       </KeyboardAvoidingView>
